8526f4b986a96c2f25ce4dc9220f876bffd11311
Tatiana <batagaeva.ta@gmail.com>
2019-05-13 03:42:11 +0800

    адаптив до 320

--- js/paralax.js
+++ js/paralax.js
@@ -3,7 +3,6 @@ window.addEventListener('DOMContentLoaded', function () {
   var area = document.querySelector('.elk'),
     elk = document.querySelector('.elk-svg-pattern__img'),
     grow = document.querySelector('.elk-svg-pattern__grow');
-  parallax(area, elk, grow);
 
   function parallax(area, obj1, obj2) {
     area.addEventListener('mousemove', function (e) {
@@ -15,42 +14,8 @@ window.addEventListener('DOMContentLoaded', function () {
       console.log(str);
       obj1.style.transform = str;
       obj2.style.transform = str;
-    })
+    });
   }
 
-  
-  var phone = document.querySelector('.header-phone'),
-  menu = document.querySelector('.nav-items'),
-  panel = document.querySelector('.header-panel');
-  
-  toggleMenu();
-  window.addEventListener('resize', function () {
-    toggleMenu();
-  });
-
-  function toggleMenu() {
-    if (window.innerWidth <= 450) {
-      panel.appendChild(phone);
-    } else {
-      menu.appendChild(phone);
-    }
-  }
-
-  var btn_toggle = document.querySelector('.header__toggle');
-
-  btn_toggle.addEventListener('click', function(){
-    if (window.innerWidth <= 450) {
-      if (parseInt(getComputedStyle(menu).top) < 0){
-        menu.classList.add('menu-down');
-        // menu.style.top = "0";
-
-        panel.style.justifyContent = "space-between";
-      } else {
-        menu.classList.remove('menu-down');
-
-        // menu.style.top = "";
-        panel.style.justifyContent = "flex-start";
-      }
-    }
-  })
+  parallax(area, elk, grow);
 });
---
+++ js/toggleMenu.js
@@ -0,0 +1,64 @@
+
+  var body = document.querySelector('body'),
+  phone = document.querySelector('.header-phone'),
+  menu = document.querySelector('.nav-items'),
+  panel = document.querySelector('.header-panel');
+
+toggleMenu();
+window.addEventListener('resize', function () {
+  toggleMenu();
+});
+
+function toggleMenu() {
+  if (window.innerWidth <= 450) {
+    panel.appendChild(phone);
+  } else {
+    menu.appendChild(phone);
+  }
+}
+
+var btn_toggle = document.querySelector('.header__toggle');
+
+function openMenu(){
+  menu.classList.add('menu-down');
+  btn_toggle.classList.add('toggle-transform');
+  panel.style.justifyContent = "space-between";
+}
+function closeMenu(){
+  menu.classList.remove('menu-down');
+  btn_toggle.classList.remove('toggle-transform');
+  panel.style.justifyContent = "flex-start";
+}
+
+  function parentsOfElements(elem, cl) {
+    let curr = elem;
+    while (curr != null) {
+      if (curr.classList.contains(cl)) {
+        return true;
+      }
+      curr = curr.parentElement;
+    }
+    return false;
+  }
+
+btn_toggle.addEventListener('click', function () {
+  if (window.innerWidth <= 450) {
+    if (parseInt(getComputedStyle(menu).top) < 0) {
+      openMenu();
+    } else {
+      closeMenu();
+    }
+  }
+});
+
+body.addEventListener("click", function (e) {
+  if (window.innerWidth <= 450) {
+    if (parseInt(getComputedStyle(menu).top) >= 0) {
+      if (!parentsOfElements(e.target, "nav-items") &&
+        !e.target.classList.contains("header__toggle") &&
+        !e.target.classList.contains("header-phone")) {        
+          closeMenu();
+      }
+    }
+  }
+});
