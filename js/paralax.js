80c7a854687f16c5020f331214cb79b824c5c1ba
Tatiana <batagaeva.ta@gmail.com>
2019-05-12 12:22:13 +0800

    адаптив до 1252

--- js/paralax.js
+++ js/paralax.js
@@ -1,25 +1,21 @@
 window.addEventListener('DOMContentLoaded', function () {
   'use strict';
-  function parallax(e, target, layer) {
-    var strength = 20;
-    var layer_c = strength / layer;
-    var x = (window.width - target.offsetWidth) / 2 - (e.pageX - (window.width / 2)) / layer_c;
-    var y = (window.height - target.offsetHeight) / 2 - (e.pageY - (window.height / 2)) / layer_c;
-    target.style.transform = "translate(" + x + "," + y + ")";
+  var area = document.querySelector('.elk'),
+    elk = document.querySelector('.elk-svg-pattern__img'),
+    grow = document.querySelector('.elk-svg-pattern__grow');
+  parallax(area, elk, grow);
+
+  function parallax(area, obj1, obj2) {
+    area.addEventListener('mousemove', function (e) {
+      var change;
+      var xpos = e.clientX;
+      var ypos = e.clientY;
+      var xpos = xpos * 2;
+      ypos = ypos * 2;
+      var str = 'translate(' + ((0 - (xpos / 80)) + "px, ") + ((0 - (ypos / 50)) + "px)");
+      console.log(str);
+      obj1.style.transform = str;
+      obj2.style.transform = str;
+    })
   }
-  window.addEventListener('mousemove', function(e){
-		parallax(e, document.getElementsByClassName('elk-svg-pattern__img'), 1);
-  });
-  // mousemove(function(e) {
-	// 	parallax(e, document.getElementsByClassName('elk-svg-pattern__img'), 1);
-		// parallax(e, document.getElementById('c2'), 2);
-		// parallax(e, document.getElementById('c3'), 3);
-		// parallax(e, document.getElementById('c4'), 4);
-		// parallax(e, document.getElementById('c5'), 5);
-		// parallax(e, document.getElementById('c6'), 6);
-		// parallax(e, document.getElementById('c7'), 7);
-		// parallax(e, document.getElementById('c8'), 8);
-	// });
 });
-
-
